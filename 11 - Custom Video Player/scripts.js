6cde7eba8177ffaf8d07daf58d0f3c7cb99a6128
amyspencerproject <amy@spencerproject.com>
2021-10-30 10:40:03 -0600

    Coded play and pause toggle button

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,29 @@
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const range = document.querySelectorAll('.player__slider');
+
+/* Build our functions */
+function togglePlay () {
+    if (video.paused) /* .paused intrinsic to video element */ {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+function updateButton () {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+};
+
+/* Hook up our event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
