8b39627f8adbf379d3ed9b39362612800debf96f
amyspencerproject <amy@spencerproject.com>
2021-11-01 10:38:20 -0600

    Added functionality for volume and video speed sliders and video progress bar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -21,9 +21,43 @@ function updateButton () {
     toggle.textContent = icon;
 };
 
+function skip () {
+    video.currentTime += parseFloat(this.dataset.skip);
+};
+
+function handleRangeUpdate () {
+    video[this.name] = this.value;
+};
+
+function handleProgress () {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+};
+
+function scrub (e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+};
+
 /* Hook up our event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
+video.addEventListener('timeupdate', handleProgress);
+
+
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+range.forEach (range => range.addEventListener('change', handleRangeUpdate));
+// range.forEach (range => range.addEventListener('mousemove', handleRangeUpdate));
+//could add a flag here for only changing value when mouse is clicked down, similar to Canvas exercise
+
+let mousedown = false;
+progress.addEventListener ('click', scrub);
+
+progress.addEventListener ('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener ('mousedown', () => mousedown = true);
+progress.addEventListener ('mouseup', () => mousedown = false);
